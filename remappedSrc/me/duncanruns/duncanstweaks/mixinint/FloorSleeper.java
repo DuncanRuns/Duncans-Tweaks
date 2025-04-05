757027021d4d7d5271b626a6d56b8b8785d5be5b
Duncan <59705125+DuncanRuns@users.noreply.github.com>
2025-04-05 13:03:32 -0300

    Delete remappedSrc directory

--- remappedSrc/me/duncanruns/duncanstweaks/mixinint/FloorSleeper.java
+++
@@ -1,7 +0,0 @@
-package me.duncanruns.duncanstweaks.mixinint;
-
-public interface FloorSleeper {
-    boolean duncansTweaks$isFloorSleeping();
-
-    void duncansTweaks$setFloorSleeping(boolean b);
-}
